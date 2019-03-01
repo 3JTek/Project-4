2f0116b68a6fa9c4e6d4ad327671aecab480b0c9
3jtechno <3j.techno@gmail.com>
2019-03-01 13:59:05 +0000

    Change DATABASEURI to DATABASE_URL

--- config/environment.py
+++ config/environment.py
@@ -1,5 +1,5 @@
 import os
 
 secret = os.getenv('SECRET', 'shh, this is a secret')
-dburi = os.getenv('DATABSEURI', 'postgres://localhost:5432/gather')
+dburi = os.getenv('DATABASE_URL', 'postgres://localhost:5432/gather')
 domainUrl = os.getenv('DEV_DOMAIN', 'HEROKU_URL')
