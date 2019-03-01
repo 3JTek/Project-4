e88a33e8b6d99ef4a8cae9bba73e1913090cf95a
3jtechno <3j.techno@gmail.com>
2019-03-01 14:10:53 +0000

    fix HEROKU_URL env variable

--- config/environment.py
+++ config/environment.py
@@ -2,4 +2,6 @@ import os
 
 secret = os.getenv('SECRET', 'shh, this is a secret')
 dburi = os.getenv('DATABASE_URL', 'postgres://localhost:5432/gather')
-domainUrl = os.getenv('DEV_DOMAIN', 'HEROKU_URL')
+domainUrl = os.getenv('HEROKU_URL', 'http://localhost:8000/api/')
+
+print(domainUrl)
