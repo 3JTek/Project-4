024dc11250897726fd3e2653832ec04115d7b1c6
3jtechno <3j.techno@gmail.com>
2019-03-01 10:41:03 +0000

    adding default route

--- config/routes.py
+++ config/routes.py
@@ -1,3 +1,4 @@
+import os
 from app import app
 from controllers import auth, categories, users, sales
 
@@ -5,3 +6,12 @@ app.register_blueprint(auth.api, url_prefix='/api')
 app.register_blueprint(categories.api, url_prefix='/api')
 app.register_blueprint(users.api, url_prefix='/api')
 app.register_blueprint(sales.api, url_prefix='/api')
+
+@app.route('/', defaults={'path': ''}) # homepage
+@app.route('/<path:path>') # any other path
+def catch_all(path):
+
+    if os.path.isfile('dist/' + path): # if path is a file, send it back
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html') # otherwise send back the index.html file
