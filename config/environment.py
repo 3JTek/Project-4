189b84b8da589b1b32dfcd16fb951833029bf616
3jtechno <3j.techno@gmail.com>
2019-03-01 12:56:24 +0000

    added a domainUrl env variable

--- config/environment.py
+++ config/environment.py
@@ -2,3 +2,4 @@ import os
 
 secret = os.getenv('SECRET', 'shh, this is a secret')
 dburi = os.getenv('DATABSEURI', 'postgres://localhost:5432/gather')
+domainUrl = os.getenv('DEV_DOMAIN', 'HEROKU_URL')
